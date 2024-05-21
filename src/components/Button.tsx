2323a90f4efc687537a3c018db35b9ec01687199
Zagaz <andre_ranulfo@hotmail.com>
2024-05-21 14:25:37 -0300

    chore: Refactor Button component to improve readability and remove unnecessary code

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,27 +1,21 @@
 import React from "react";
-import {useState} from "react";
+import { useState } from "react";
 
 const [buttonStyle, setButtonStyle] = useState<string>("light");
 
-
-
-
 interface ButtonProps {
      value: string;
-     style : string;
+     style: string;
 }
 
-function debug():void {
+function debug(): void {
      console.log("Button clicked");
 }
 
-const Button: React.FC<ButtonProps> = ({ value  , style }) => {
+const Button: React.FC<ButtonProps> = ({ value, style }) => {
      return (
           <>
-          {
-               style && value.length > 0 ? <h1> {value} </h1> : <h1> No value </h1>
-          }
-          <button className = {`button-${buttonStyle}`} onClick={() => debug()}> {value} </button>
+               <button className={`button-${buttonStyle}`} onClick={() => debug()}> {value} </button>
           </>
      );
 }
