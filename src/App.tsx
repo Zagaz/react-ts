eb3d49d4486432a1c36b29e26cf8ec73181ab9e5
Zagaz <andre_ranulfo@hotmail.com>
2024-05-22 14:21:54 -0300

    Refactor App.tsx to include Menu component and PageTest component

--- src/App.tsx
+++ src/App.tsx
@@ -4,59 +4,23 @@ import './AppSecond.css'
 import Button from './components/Button';
 import Title from './components/Title';
 import Test from './components/Test';
-import { useState } from 'react';
-
+import PageTest from './components/PageTest';
+import Menu from './components/Menu';
 
 function App() {
 
-  const [num, setNum] = useState<number>(0);
-
-
- // function handleClick return number
-
- function addNumber(): void {
-   setNum(num + 1);
-
- }
-
- function subNumber():void {
-  setNum(num - 1);
-
-}
-
- function handleClick2(): void {
-    const date = new Date();
-    const currentDate = date.toDateString();
-   alert (`Void function and vale at: ${currentDate}`)
-   alert (`The type of num is: ${stringfyNum()}`)
-  }
-  
-   function stringfyNum(): string {
-    return (typeof num)
-  }
-
-
-
   return (
-    <div className="App">
-      <header className="App-header ">
-        <div className='app-container'>
-          <div className='app-container__top'>
-        <Title title = "Hello World" gender = "male" age = {num}/>
-        </div>
-        
-        <div className='app-container__middle'>
-        <Button  style = "success" onClick = {addNumber}>+1</Button>
-        <Button  style = "danger" onClick = {subNumber}>-1</Button>
-        </div>
+    <>
+      <div className="App">
+        <header className="App-header ">
+          <Menu />
+          <div className='app-container'>
 
-        <div className='app-container__bottom'>
-        <Button  onClick = {handleClick2}>Alert Message</Button>
-        </div>
-        
-        </div>
-      </header>
-    </div>
+            <PageTest />
+          </div>
+        </header>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Menu.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react"
+import React from 'react'
+
+
+function Menu() {
+  return (
+    <>
+    <nav className='menu'>
+
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+
+    </nav>
+
+    
+         
+     
+    </>
+  )
+}
+
+export default Menu
