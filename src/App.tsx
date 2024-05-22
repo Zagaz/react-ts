fdb99461e9816dc03a719d5a095a73db293d4b3f
Zagaz <andre_ranulfo@hotmail.com>
2024-05-22 20:26:29 -0300

    feat: Add lazy loading for images and improve resource type buttons in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,17 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 // import Hero from './components/Hero'
-import Card from './components/Card'
-
-import { useEffect, useState } from 'react'
+import PageUseEffect from './components/PageUseEffect'
 
 function App() {
 
-  const [resourceType, setResourceType] = useState<string>('posts')
-  const [items, setItems] = useState([])
 
-  useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-      .then(response => response.json())
-      .then(json => setItems(json))
-  }, [resourceType])
 
 
   return (<>
     <Navbar />
-    <div className='buttons-container'>
-      <button onClick={() => setResourceType('posts')}> Posts </button>
-      <button onClick={() => setResourceType('users')}> Users </button>
-      <button onClick={() => setResourceType('comments')}> Comments </button>
-    </div>
-    <div className='page-title'>
-    <h1>{resourceType}</h1>
-    </div>
-    <div className='card-container'>
-    {
-      resourceType === 'posts' && items.map((item: any) => {
-        return (
-          <>
-             
-          <Card key={item.id} title={item.title} body={item.body} 
-          />
-          </>
+    <PageUseEffect />
 
-
-        )
-      }
-      )}
-
-      {
-      resourceType === 'users' && items.map((item: any) => {
-        return (
-          <>
-    
-          <Card key={item.id} name={item.name} mail={item.email}
-          />
-          </>
-        )
-      }
-      )}
-
-      {
-      resourceType === 'comments' && items.map((item: any) => {
-        return (
-          <>
-    
-          <Card key={item.id} name={item.name} mail={item.email}
-          />
-          </>
-        )
-      }
-      )
-      }
-      </div>
 
 
   </>
