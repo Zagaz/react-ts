36b37f5570d27c132786ba6d65ae570530af1e24
Zagaz <andre_ranulfo@hotmail.com>
2024-05-22 15:36:34 -0300

    Refactor code structure and remove unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,9 @@
-import React from 'react';
-import './App.css';
-import './AppSecond.css'
-import Button from './components/Button';
-import Title from './components/Title';
-import Test from './components/Test';
-import PageTest from './components/PageTest';
-import Menu from './components/Menu';
+import React from 'react'
 
-function App() {
-
+const App = () => {
   return (
-    <>
-      <div className="App">
-        <header className="App-header ">
-          <Menu />
-          <div className='app-container'>
-
-            <PageTest />
-          </div>
-        </header>
-      </div>
-    </>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily : {
+        sans : ['Roboto' , 'sans-serif']
+      },
+      gridAutoColumns:{
+        '70/30' : '70% 28%',
+      }
+    },
+  },
+  plugins: [],
+}
+
