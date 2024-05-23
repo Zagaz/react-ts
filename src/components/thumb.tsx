9a515acd751ae7fe84d53a244113ee3abee3f0a9
Zagaz <andre_ranulfo@hotmail.com>
2024-05-23 06:41:17 -0300

    Refactor Thumb component to support different image styles and aspect ratios

--- src/components/thumb.tsx
+++ src/components/thumb.tsx
@@ -4,12 +4,13 @@ import React from 'react'
 interface thumbProps{
      src : string
      alt ?: string
-     style ?: "square" | "portrait" | "landscape" 
+     style?: "success" | "danger" | "warning" | "info" | "black" | "white" | "grey"
+     aspect ?: "square" | "portrait" | "landscape"
 
 
 }
 
-const Thumb: React.FC<thumbProps> = ({ src, alt, style }) => {
+const Thumb: React.FC<thumbProps> = ({ src, alt, style, aspect }) => {
      return (
           <img className={`thumb-style-${!style ? "portrait" : style}`} 
           src={src} alt={alt}/>
