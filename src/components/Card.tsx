9b70e6de1d73fe71732a455b0191680fa14e5e28
Zagaz <andre_ranulfo@hotmail.com>
2024-05-23 22:29:04 -0300

    Refactor Card component to remove unused aspect prop

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,29 +1,25 @@
-import React from 'react'
-import Thumb from './Thumb'
-
+import React from 'react';
+import Thumb from './Thumb';
 
 interface CardProps {
-     id?: number
-     name?: string
-     title?: string
-     body?: string
-     mail?: string
-     url?: string
-     aspect?: 'square' | 'landscape' | 'portrait'
+  id?: number;
+  name?: string;
+  title?: string;
+  body?: string;
+  mail?: string;
+  url?: string;
 }
+
 const Card: React.FC<CardProps> = ({ id, name, title, body, mail, url }) => {
-     // temp string aspect 
-     var aspectString: string = "square";
-     return (
-          <>
-               <div className={`card id-card-${id}`}>
-                    {url && <Thumb src={url} alt={name} aspect="landscape" />}
-                    {name && <h1>{`${name}`}</h1>}
-                    {title && <h1>{`${title}`}</h1>}
-                    {body && <p>{`${body}`}</p>}
-                    {mail && <p>{`Mail: ${mail}`}</p>}
-               </div>
-          </>
-     )
+  return (
+    <div className={`card id-card-${id}`}>
+      {url && <Thumb src={url} alt={name} aspect="landscape" />}
+      {name && <h2>{name}</h2>}
+      {title && <h2>{title}</h2>}
+      {body && <p>{body}</p>}
+      {mail && <p>Mail: {mail}</p>}
+    </div>
+  );
 }
-export default Card
+
+export default Card;
--- src/components/PageUseEffect.tsx
+++ src/components/PageUseEffect.tsx
@@ -1,93 +1,93 @@
-import React from 'react'
-import { useEffect, useState } from 'react';
-import Card from './Card';
-import Button from './Button';
-
+import { useEffect, useState } from "react";
+import Card from "./Card";
+import Button from "./Button";
 
 export default function PageUseEffect() {
-     const [resourceType, setResourceType] = useState<string>('posts')
-     const [items, setItems] = useState([])
+  const [resourceType, setResourceType] = useState<string>("posts");
+  const [items, setItems] = useState([]);
 
-     useEffect(() => {
-          fetchAPI();
+  useEffect(() => {
+    fetchAPI();
+  }, [resourceType]);
 
-     }, [resourceType])
+  async function fetchAPI() {
+    let url = `https://jsonplaceholder.typicode.com/${resourceType}`;
+    try {
+      const asyncResponse = await fetch(url);
+      const response = await asyncResponse.json();
+      setItems(response);
+    } catch (e: any) {
+      alert("Error: " + e.message);
+    }
+  }
 
-     async function fetchAPI() {
-          let url = `https://jsonplaceholder.typicode.com/${resourceType}`;
-          try {
-               const asyncResponse = await fetch(url)
-               const response = await asyncResponse.json()
-               setItems(response)
-          } catch (e) {
-               console.log("error")
-               console.log(e)
-          }
-     }
-     return (
-          <div>
-               <div className='buttons-container'>
-                    <Button onClick={() => setResourceType('posts')}>Posts</Button>
-                    <Button onClick={() => setResourceType('comments')}>Comments</Button>
-                    <Button onClick={() => setResourceType('users')}>Users</Button>
-                    <Button onClick={() => setResourceType('albums/100/photos')}>Photos</Button>
-               </div>
-               <div className='page-title'>
-                    <h1>{resourceType}</h1>
-               </div>
-               <div className='card-container'>
-                 
-                    {
-                         resourceType === 'posts' && items.map((item: any) => {
-                              return (
-                                   <>
+  function changeItem(item: string): void {
+    setResourceType(item);
+  }
+  function renderCards(resourceType: string, items: any[]) {
+    switch (resourceType) {
+      case "posts":
+        return items.map((item: any) => (
+          <Card key={item.id} title={item.title} body={item.body} />
+        ));
+      case "users":
+        return items.map((item: any) => (
+          <Card key={item.id} name={item.name} mail={item.email} />
+        ));
+      case "comments":
+        return items.map((item: any) => (
+          <Card key={item.id} name={item.name} mail={item.email} />
+        ));
+      default:
+        return null;
+    }
+  }
+  let btItems: [string, string][];
+  btItems = [
+    ["posts", "Posts"],
+    ["comments", "Comments"],
+    ["users", "Users"],
+    ["albums/100/photos", "Photos"],
+  ];
+  return (
+    <div>
+      <div className="buttons-container">
+        {btItems.map((item: [string, string]) => {
+          return (
+            <Button key={item[0]} onClick={() => changeItem(item[0])}>
+              {" "}
+              {item[1]}{" "}
+            </Button>
+          );
+        })}
+      </div>
+      <div className="page-title">
+        <h1>
+          {resourceType === "albums/100/photos" ? "photos" : resourceType}
+        </h1>
+      </div>
+      <div className="card-container">
+        {resourceType === "posts" &&
+          items.map((item: any) => {
+            return <Card key={item.id} title={item.title} body={item.body} />;
+          })}
 
-                                        <Card key={item.id} title={item.title} body={item.body}
-                                        />
-                                   </>
+        {resourceType === "users" &&
+          items.map((item: any) => {
+            return <Card key={item.id} name={item.name} mail={item.email} />;
+          })}
 
-
-                              )
-                         }
-                         )
-                    }
-
-                    {
-                         resourceType === 'users' && items.map((item: any) => {
-                              return (
-                                   <>
-
-                                        <Card key={item.id} name={item.name} mail={item.email}
-                                        />
-                                   </>
-                              )
-                         }
-                         )
-                    }
-
-                    {
-                         resourceType === 'comments' && items.map((item: any) => {
-                              return (
-                                   <>
-
-                                        <Card key={item.id} name={item.name} mail={item.email}
-                                        />
-                                   </>
-                              )
-                         }
-                         )
-                    }
-                    {
-                         resourceType === 'albums/100/photos' && items.map((item: any) => {
-                              return (
-                                   <>
-                                        <Card key={item.id} url={item.thumbnailUrl} name={item.title} />
-                                   </>
-                              )
-                         }
-                         )
-                    }
-               </div>
-          </div>
-     )
+        {resourceType === "comments" &&
+          items.map((item: any) => {
+            return <Card key={item.id} name={item.name} mail={item.email} />;
+          })}
+        {resourceType === "albums/100/photos" &&
+          items.map((item: any) => {
+            return (
+              <Card key={item.id} url={item.thumbnailUrl} name={item.title} />
+            );
+          })}
+      </div>
+    </div>
+  );
 }
