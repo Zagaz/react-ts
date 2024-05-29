4b6d747f743db16dddaad31930f7ed7185d2d7b9
Zagaz <andre_ranulfo@hotmail.com>
2024-05-29 13:36:26 -0300

    Refactor PageUseEffect component to remove unused code and improve resource type buttons and lazy loading for images

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-// import Hero from './components/Hero'
 import PageUseEffect from './components/PageUseEffect'
 
 function App() {
 
-
-
-
   return (<>
-    <Navbar />
     <PageUseEffect />
-
-
-
   </>
   )
 }
